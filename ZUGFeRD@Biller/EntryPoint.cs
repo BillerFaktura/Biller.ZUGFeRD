01c885d4abb82572d26c2fe742855ae8aaf900be
Igor Lückel <igor.lueckel@googlemail.com>
2014-09-03 16:52:14 +0200

    Initial commit

---
+++ ZUGFeRD@Biller/EntryPoint.cs
@@ -0,0 +1,49 @@
+﻿using s2industries.ZUGFeRD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZUGFeRD_Biller
+{
+    public class EntryPoint : Biller.UI.Interface.IPlugIn
+    {
+        private Manager.ControlManager controlManager;
+
+        public EntryPoint(Biller.UI.ViewModel.MainWindowViewModel parentViewModel)
+        {
+            this.ParentViewModel = parentViewModel;
+        }
+
+        public Biller.UI.ViewModel.MainWindowViewModel ParentViewModel { get; private set; }
+
+        public string Name
+        {
+            get { return "ZUGFeRD @ Biller"; }
+        }
+
+        public string Description
+        {
+            get { return ""; }
+        }
+
+        public double Version
+        {
+            get { return 1.20140903; }
+        }
+
+        public void Activate()
+        {
+            if (controlManager == null)
+            {
+                controlManager = new Manager.ControlManager(ParentViewModel);
+            }
+        }
+
+        public List<Biller.UI.Interface.IViewModel> ViewModels()
+        {
+            return new List<Biller.UI.Interface.IViewModel>();
+        }
+    }
+}
---
+++ ZUGFeRD@Biller/Manager/ControlManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZUGFeRD_Biller.Manager
+{
+    public class ControlManager : Biller.UI.Interface.IEditObserver
+    {
+        Biller.UI.ViewModel.MainWindowViewModel mainWindowViewModel;
+        public ControlManager(Biller.UI.ViewModel.MainWindowViewModel mainWindowViewModel)
+        {
+            this.mainWindowViewModel = mainWindowViewModel;
+            mainWindowViewModel.DocumentTabViewModel.RegisterObserver(this);
+        }
+
+        public void ReceiveArticleEditViewModel(Biller.UI.ArticleView.Contextual.ArticleEditViewModel articleEditViewModel)
+        {
+            //
+        }
+
+        public void ReceiveCustomerEditViewModel(Biller.UI.CustomerView.Contextual.CustomerEditViewModel customerEditViewModel)
+        {
+            //
+        }
+
+        public void ReceiveDocumentEditViewModel(Biller.UI.DocumentView.Contextual.DocumentEditViewModel documentEditViewModel)
+        {
+            documentEditViewModel.EditContentTabs.Add(new Control.EditTab());
+        }
+    }
+}
